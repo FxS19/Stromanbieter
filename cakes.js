bc692dc5ee6d456b687d2dac81829423a17cd63a
Felix Schmidt <felix.schmidt.1@gmx.de>
2020-10-13 16:24:33 +0200

    Vorlesung Web-Programmierung 20201013

---
+++ cakes.js
@@ -0,0 +1,7 @@
+const cakes = [
+    {id: 1, name: "Apfelkuchen", weight: 500, uom: "g"},
+    {id: 2, name: "Erdbeerkuchen", weight: 1500, uom: "g"},
+    {id: 3, name: "Smurfcake", weight: 1, uom: "g"},
+  ];
+  
+  module.exports = cakes;
---
+++ server.js
@@ -0,0 +1,55 @@
+// server.js
+// where your node app starts
+
+// we've started you off with Express (https://expressjs.com/)
+// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+let cakes = require ("./cakes");
+
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+app.use(bodyParser.json())
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/cakes", (request, response) => {
+  response.json(cakes);
+});
+
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/cakes/:id", (request, response) => {
+  console.log(request.params);
+  let mycake = cakes.find(element => element.id == parseInt(request.params.id));
+  if (mycake != null) {
+    response.json(mycake);
+  }else{
+    response.status(404).send({error: "Not found"});
+  }
+});
+
+app.post('/cakes',  (request, response) => {
+    console.log(request.body);
+    try {
+      const cake = {
+        id: Math.max(...cakes.map(element => element.id))+1,
+        name: request.body.name,
+        weight: request.body.weight,
+        uom: request.body.uom
+      }
+      cakes.push(cake);
+      response.json(cake);
+    } catch (error) {
+      response.status("400").send({error: "Wrong message"});
+    }
+    
+});
+
+
+
+// listen for requests :)
+const listener = app.listen(8080, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
