aa36c5e4b052e464ed92cd299b15ac31eacecfd1
Felix Schmidt <felix.schmidt.1@gmx.de>
2020-11-11 16:29:54 +0100

    /rates interface

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const importer =require("./import");
 //importer.importData();
 
 const provideDatabase = require('./database');
+const parse = require("csv-parse");
 const database = provideDatabase();
 
 // make all the files in 'public' available
@@ -24,6 +25,35 @@ app.use(bodyParser.json())
 /*app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });*/
+
+/**
+ * Schnittstelle für /rates
+ */
+app.get("/rates", async (request, response) => {
+  const zipIsValide = /^\d{5}$/.test(request.query.zipCode); //5-Stellige PLZ
+  const consumptionIsValide = /^[0-9,\.]+$/.test(request.query.consumption);//Zahl, fkz[,.]
+  const zip = parseInt(request.query.zipCode);
+  const consumption = parseFloat(request.query.consumption.replace(",", "."));
+  if (zipIsValide && consumptionIsValide && !isNaN(consumption)){
+    const db = await database;
+    const tarife = await db.all("SELECT tp.tarif_plz_id, t.name, tp.fixkosten, tp.variablekosten FROM tarif t, tarif_plz tp WHERE t.tarif_id = tp.tarif_id and plz = ?", zip);
+    let calc = tarife.map((tarif) => {
+      return {
+        "id": tarif.tarif_plz_id,
+        "title": tarif.name,
+        "zipCode": request.query.zipCode,
+        "pricePerUnit": tarif.variablekosten,
+        "basicPrice": tarif.fixkosten,
+        "consumption": consumption,
+        "calculatedPricePerYear": tarif.fixkosten + tarif.variablekosten * consumption,
+      };
+    });
+    calc = calc.sort((lhs, rhs) => lhs.calculatedPricePerYear - rhs.calculatedPricePerYear);
+    response.json(calc);
+  }else{
+    response.status(404).send({error: "bad values"});
+  }
+});
 
 // listen for requests :)
 const listener = app.listen(8080, () => {
